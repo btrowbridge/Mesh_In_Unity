888eade18e69ab2a4e825c37183a3e596bc6532f
btrowbridge <bradley.trowbridge@yahoo.com>
2015-09-28 20:06:25 -0400

    There are data membors for the sensor data and a node to MongoDB relationship set up, there is issues with one line of code with type errors. will look for answers later

---
+++ Assets/Scripts/MongoDBServer.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+public class MongoDBServer : MonoBehaviour {
+
+
+	private List<Node>[] nodes;					//list of nodes on server
+	private MongoServerSettings settings;		//server settings
+	private MongoServer server;					//server
+					
+	public int delay = 10;						// delay of how old you want your data (seconds)
+
+	// Use this for initialization
+	void Start () {
+
+		// Create server settings to pass connection string, timeout, etc.
+		settings = new MongoServerSettings();
+		settings.Server = new MongoServerAddress("localhost", 27017);
+
+		// Create server object to communicate with our server
+		server = new MongoServer(settings);
+
+
+	}
+	/*
+	public void addToNodeList(Node node)
+	{
+	
+	}
+	*/
+	//grab data from specific node
+	public void updateNode(Node node)
+	{
+		//get DB from server
+		var _database = server.GetDatabase("sensornetwork");
+		//get collection from server
+		var collection = _database.GetCollection<BsonDocument> ("sensordata");
+		//create a builder to build query
+		var builder = Builders<BsonDocument>.Filter;
+		//look for Bsondocument with same name as node
+		var filter = builder.Eq ("name", (string)node.getName ()) & builder.Eq ("timestamp",(int)(node.getTimeStamp () + delay));
+		BsonDocument result = collection.Find (filter).ToBsonDocument ();
+
+		//parse the bson document and update the nnode data members
+		node.pointToSensor ().parseBsonToSensorData (result);
+
+	}
+
+
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+
+	}
+
+}
