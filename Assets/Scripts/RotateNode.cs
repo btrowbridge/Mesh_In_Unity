01a367bd3f4259e79e72d6a051f40a3393679137
John McCormack <mccormack.wgsi@gmail.com>
2015-10-03 16:34:09 -0400

    Starting to work on animating the Text to display node data

--- Assets/Scripts/RotateNode.cs
+++ Assets/Scripts/RotateNode.cs
@@ -4,25 +4,24 @@ using System.Collections;
 public class RotateNode : MonoBehaviour
 {
 
-    public float base_speed = 1.5f;
-    public float speed_increase = 0.05f;
-    public float current_speed;
-    public float rotate_speed = 20.0f;
-    private float min;
-    private float max;
-    private float units = 1.5f;
-    private Vector3 direction;
+    public Vector3 startMarker;
+    public Vector3 endMarker;
+    public float distance = 3.0F;
+    public float speed = 1.0F;
+    private float startTime;
+    private float journeyLength;
+
+    public float rotate_speed = 25.0f;
+
 
     // Use this for initialization
     void Start()
     {
-        max = transform.position.y;
-        min = transform.position.y - units;
-        current_speed = base_speed;
-
-        direction = Vector3.down;
-        print(max);
-        print(min);
+        startTime = Time.time;
+        startMarker = transform.position;
+        endMarker = startMarker;
+        endMarker.Set(endMarker.x, endMarker.y - distance, endMarker.z);
+        journeyLength = Vector3.Distance(startMarker, endMarker);
 
 
     }
@@ -30,20 +29,17 @@ public class RotateNode : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
-        if (transform.position.y >= max)
+        float distCovered = (Time.time - startTime) * speed;
+        print(distCovered);
+        float fracJourney = distCovered / journeyLength;
+        transform.position = Vector3.Lerp(startMarker, endMarker, fracJourney);
+        if (distCovered >= distance)
         {
-            current_speed = base_speed;
-            direction = Vector3.down;
+            Vector3 holdMarker = startMarker;
+            startMarker = endMarker;
+            endMarker = holdMarker;
+            startTime = Time.time;
         }
-
-        if (transform.position.y <= min)
-        {
-            current_speed = base_speed;
-            direction = Vector3.up;
-        }
-        current_speed = current_speed + speed_increase;
-        transform.Translate(direction * current_speed * speed_increase * Time.deltaTime);
         transform.Rotate(0, Time.deltaTime * rotate_speed, 0);     
 
     }
