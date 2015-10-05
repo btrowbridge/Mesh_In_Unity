ae223af49dcb9e3663411602cd27ad1825b6b2d5
John McCormack <mccormack.wgsi@gmail.com>
2015-10-04 22:32:04 -0400

    switched to a texture dictionary

--- Assets/Scripts/NodeDataDisplay.cs
+++ Assets/Scripts/NodeDataDisplay.cs
@@ -1,28 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class NodeDataDisplay : MonoBehaviour {
     //public GameObject TextCamera;
     // Use this for initialization
     public bool hasCompass = true;
+	public bool hasTherm = true;
     //public bool hasMagnetometer = true;
     //public bool hasBarometer = true;
     //public bool hasGPS = true;
     public int compass = 90;
+	public int temp = 100;
     public Material CompassTex;
+	public Material ThermTex;
     public GameObject Logo;
+	private Material material;
+
+	private Dictionary<string, Material> Logos;
 
     void Start () {
-        
+		Logos = new Dictionary<string,Material > ();
+		Logos.Add ("compass", CompassTex);
+		Logos.Add ("thermometer", ThermTex);
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () {; 
 	    if (hasCompass == true)
         {
             GetComponentInChildren<DynamicTyping>().write_this = "Compass Heading=" + compass.ToString() + "Degrees";
-            Logo.GetComponent<MeshRenderer>().sharedMaterial = CompassTex;
-            hasCompass = false;
-        }
+			Logos.TryGetValue("compass", out material);
+			Logo.GetComponent<MeshRenderer>().sharedMaterial = material;
+        	//Below line for testing only, remove later
+			hasCompass = false;
+		}
+		if (hasTherm == true) {
+			GetComponentInChildren<DynamicTyping>().write_this = "Temp =       " + compass.ToString() + "    Degrees C";
+			Logos.TryGetValue("thermometer", out material);
+			Logo.GetComponent<MeshRenderer>().sharedMaterial = material;
+			//Below line for testing only, remove later
+			hasTherm = false;
+		}
+
 	}
 }
