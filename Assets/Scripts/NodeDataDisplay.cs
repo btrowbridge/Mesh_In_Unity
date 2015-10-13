db0d4dc152093aa8027fb2100f0d5889be781612
John McCormack <mccormack.wgsi@gmail.com>
2015-10-13 15:42:02 -0400

    The Prefab nodes can now send dummy data, which is dispalyed on the screen in the node prototyping room

--- Assets/Scripts/NodeDataDisplay.cs
+++ Assets/Scripts/NodeDataDisplay.cs
@@ -4,58 +4,92 @@ using System.Collections.Generic;
 
 public class NodeDataDisplay : MonoBehaviour {
     //public GameObject TextCamera;
-    // Use this for initialization
-    public bool hasCompass = false;
-	public bool hasTherm = false;
+    //Use this for initialization
+    public bool hasCompass = true;
+	public bool hasTherm = true;
     //public bool hasMagnetometer = true;
     //public bool hasBarometer = true;
     //public bool hasGPS = true;
-    public double compass = 90;
-	public double temp = 100;
+
+    //Temporary Values for creating debugging
+    //Should be removed and made into a separate Test
+    //Data set ASAP.
+    public double compass;
+	public double temp;
+
+    //Materials for Dictionary
+    //Need to manually get Files 
+    //From Inspector 
     public Material CompassTex;
 	public Material ThermTex;
-    public GameObject Logo;
-	private Material material;
+
+    //Material Dictionary
+    public Dictionary<string, Material> Logos;
+
+
+    public GameObject LogoBox;
+	public Material material;
     private Node node;
 	public GameObject CurrentNodeObject;
 
     // cycle will count upwards until reaching the last
     // sensor and then reset to 0. 
-    private int cycle; 
+    private int cycle;
 
-	private Dictionary<string, Material> Logos;
+    private SmartTextMesh smartTextMesh;
 
     void Start () {
-		Logos = new Dictionary<string,Material > ();
-        node = FindObjectOfType<Node>();
-        compass = node.pointToSensor().compSensor.orientation;
-		Logos.Add ("compass", CompassTex);
+
+        smartTextMesh = GetComponentInChildren<SmartTextMesh>();
+
+        //node = FindObjectOfType<Node>();
+        //compass = node.pointToSensor().compSensor.orientation;
+
+        //Build Dictionary of Textures
+        Logos = new Dictionary<string, Material>();
+        Logos.Add ("compass", CompassTex);
 		Logos.Add ("thermometer", ThermTex);
 
 	}
 	
 	// Update is called once per frame
-	void Update () {; 
-	    if (hasCompass == true)
+	void Update () {;
+        if (CurrentNodeObject)
         {
-            GetComponentInChildren<DynamicTyping>().write_this = "Compass Heading=" + compass.ToString() + "Degrees";
-			Logos.TryGetValue("compass", out material);
-			Logo.GetComponent<MeshRenderer>().sharedMaterial = material;
-        	//Below line for testing only, remove later
-			hasCompass = false;
-		}
-		if (hasTherm == true) {
-			GetComponentInChildren<DynamicTyping>().write_this = "Temp =       " + compass.ToString() + "    Degrees C";
-			Logos.TryGetValue("thermometer", out material);
-			Logo.GetComponent<MeshRenderer>().sharedMaterial = material;
-			//Below line for testing only, remove later
-			hasTherm = false;
-		}
+            if (hasCompass == true)
+            {
+                smartTextMesh.UnwrappedText = "Compass Heading=\n" + compass.ToString() + "\nDegrees";
+                smartTextMesh.NeedsLayout = true;
+                //Logos.TryGetValue("compass", out material);
+                material = Logos["compass"];
+                LogoBox.GetComponent<MeshRenderer>().sharedMaterial = material;
+                //Below line for testing only, remove later
+                print(compass.ToString());
+                //hasCompass = false;
+            }
+
+            if (hasTherm == true)
+            {
+                smartTextMesh.UnwrappedText = "Temp =\n" + temp.ToString() + "\nDegrees C";
+                smartTextMesh.NeedsLayout = true;
+                Logos.TryGetValue("thermometer", out material);
+                print(material.ToString());
+                LogoBox.GetComponent<MeshRenderer>().material = material;
+                //Below line for testing only, remove later
+                //hasTherm = false;
+                print(temp.ToString());
+            }
+        }
+
 	}
 
 	public void setGameObject(string newNodeName){
         print("MSG Rec");
 		CurrentNodeObject = GameObject.Find(newNodeName);
         print(CurrentNodeObject.name.ToString());
+
+        //DummyData 
+        compass = CurrentNodeObject.GetComponent<DummyData>().compass;
+        temp = CurrentNodeObject.GetComponent<DummyData>().temperature;
 	}
 }
--- Assets/Scripts/SendDataToDisplay.cs
+++ Assets/Scripts/SendDataToDisplay.cs
@@ -25,7 +25,6 @@ public class SendDataToDisplay : MonoBehaviour {
 	 */
     void OnMouseDown()
     {
-        print("Click");
         string objectID = name;
         display.setGameObject(objectID);
     }
