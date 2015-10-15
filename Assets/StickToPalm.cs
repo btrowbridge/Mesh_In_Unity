dd39d5f7cc444fa4e508c0a7c1c69522595a8b86
John McCormack <mccormack.wgsi@gmail.com>
2015-10-14 21:57:06 -0400

    Have Screen attached to Arm

---
+++ Assets/StickToPalm.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickToPalm : MonoBehaviour {
+
+    private GameObject TV;
+    private GameObject Player;
+    private Transform TVposition0;
+
+	// Use this for initialization
+    
+	void Start () {
+        TV = GameObject.FindWithTag("Screen");
+        TVposition0 = TV.transform;
+        TV.transform.rotation.SetEulerAngles(0, 90, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TV.transform.position = this.transform.position;
+        TV.transform.rotation.y = this.transform.rotation.y + 90.0f;
+	}
+
+    void OnDestroy() {
+        TV.transform.position = TVposition0.position;
+        TV.transform.rotation = TVposition0.rotation;
+    }
+}
