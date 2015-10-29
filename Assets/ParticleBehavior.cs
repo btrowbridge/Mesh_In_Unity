e70f3826e9f7c62e0d5cc17cedff742eee323927
unknown <bradleytrowbridge06@PC-MJ0169RA.floridapoly.org>
2015-10-29 16:55:27 -0400

    Working on particle physics, WALL : particles dont have rigidbodies.

---
+++ Assets/ParticleBehavior.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleBehavior : MonoBehaviour {
+
+    public ParticleSystem particleSystem;
+    public ParticleSystem.Particle[] particles;
+
+	// Use this for initialization
+	void Start () {
+        particleSystem = GetComponent<ParticleSystem>();
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int numParticles = particleSystem.GetParticles(particles);
+        for (int i = 0; i < numParticles; i++) {
+            Vector3 line = gameObject.transform.position - particles[i].position;
+            line.Normalize();
+
+            float distance = Vector3.Distance(gameObject.transform.position, particles[i].position);
+            particles[i].rigidbody.AddForce(line * 10 / distance);
+        }
+	}
+}
