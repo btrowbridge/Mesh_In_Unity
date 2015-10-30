02b408fa28e6301e5b6a5eb1a7e9349865774176
unknown <bradleytrowbridge06@PC-MJ0169RA.floridapoly.org>
2015-10-29 20:21:58 -0400

    orbital emmitters made, next control the orbit. Pizza break...

--- Assets/ParticleBehavior.cs
+++ Assets/ParticleBehavior.cs
@@ -1,27 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(ParticleSystem))]
 public class ParticleBehavior : MonoBehaviour {
 
-    public ParticleSystem particleSystem;
-    public ParticleSystem.Particle[] particles;
-
+    private Rigidbody rigidbody;
+    private ParticleSystem particleSystem;
+    private ParticleSystem.Particle[] particles;
+    private Transform orbital;
+    private Transform nodeTarget;
+    public GameObject nodeObject;
+    public float speed;
+    
 	// Use this for initialization
 	void Start () {
         particleSystem = GetComponent<ParticleSystem>();
-       
+        
+        
+        nodeTarget = nodeObject.transform;
 	}
-	
+    void Awake() {
+        rigidbody = GetComponent<Rigidbody>();
+        orbital = this.transform;
+        rigidbody.AddForce(transform.forward * speed);
+        rigidbody.AddForce(transform.up * speed);
+    }
 	// Update is called once per frame
 	void Update () {
-        int numParticles = particleSystem.GetParticles(particles);
-        for (int i = 0; i < numParticles; i++) {
-            Vector3 line = gameObject.transform.position - particles[i].position;
-            line.Normalize();
 
-            float distance = Vector3.Distance(gameObject.transform.position, particles[i].position);
-            particles[i].rigidbody.AddForce(line * 10 / distance);
-        }
+        transform.LookAt(nodeTarget);
+        
+        Vector3 line = nodeTarget.position - orbital.position;
+        line.Normalize();
+        
+        float distance = Vector3.Distance(gameObject.transform.position, nodeObject.transform.position);
+    
+        rigidbody.AddForce(line * 10 / distance);
+
+        particleSystem.startLifetime = distance / particleSystem.startSpeed;
+
 	}
+
 }
