d5f71424ad27c41a5dc2e88109f89884f100351a
unknown <bradleytrowbridge06@PC-MJ0169RA.floridapoly.org>
2015-11-02 22:02:16 -0500

    Nodes emit to other nodes

--- Assets/ParticleBehavior.cs
+++ Assets/ParticleBehavior.cs
@@ -9,9 +9,10 @@ public class ParticleBehavior : MonoBehaviour {
     private ParticleSystem particleSystem;
     private ParticleSystem.Particle[] particles;
     private Transform orbital;           //this objects transform
-    private Transform nodeTarget;        //transform of target node
-    
-    public GameObject nodeObject;        // refers to the target to be rotated around
+    private Transform nodeTarget;        // transform of target node to rotate around
+    private GameObject[] nodes;           //list of nodes in scene
+       
+    public GameObject nodeObject;        // refers to the target to be emmitted toward
     public float speed;                  //speed of orbit
     public float radius = 4;             //radius of orbit
 
@@ -30,6 +31,7 @@ public class ParticleBehavior : MonoBehaviour {
         //reference to target node for orbiting
         nodeTarget = nodeObject.transform;
 
+
         //places emmitter at radius
         transform.position = (transform.position - nodeTarget.position).normalized * radius + nodeTarget.position;
     }
@@ -44,26 +46,37 @@ public class ParticleBehavior : MonoBehaviour {
         
         
     }
-	// Update is called once per frame
-	void Update () {
-
-        //makes sure the emmitter faces node
-        transform.LookAt(nodeTarget);
-        //makes sure the emitter stays at the radius
-        transform.position = (transform.position - nodeTarget.position).normalized * radius + nodeTarget.position;
-
-        //vector pointing to the node
-        Vector3 line = nodeTarget.position - orbital.position;
-        line.Normalize(); // set to 1
-        
-        //calculate distance
+    void Update() {
+        nodes = GameObject.FindGameObjectsWithTag("node");
+        //calculate distance from eimission
         float distance = Vector3.Distance(gameObject.transform.position, nodeObject.transform.position);
-        
-        //centrifical force
-        rigidbody.AddForce(line * speed);
 
         //lifetime particle depends on object they are looking at
         particleSystem.startLifetime = distance / particleSystem.startSpeed;
+
+
+        // random node from list
+        nodeObject = nodes[Random.Range(0,nodes.Length)];
+        
+
+        //makes sure the emmitter faces node
+        transform.LookAt(nodeObject.transform);
+
+        //makes sure the emitter stays at the radius
+        transform.position = (transform.position - nodeTarget.position).normalized * radius + nodeTarget.position;
+
+        //vector pointing to the node body it is orbiting
+        Vector3 line = nodeTarget.position - orbital.position;
+        line.Normalize(); // set to 1
+      
+        
+        //centrifical force
+        rigidbody.AddForce(line * speed);
+    }
+
+	// 
+	void FixedUpdate () {
+
 
 	}
 
